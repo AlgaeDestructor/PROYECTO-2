ea9dd2eaf2f1c64e6242a27acecfc65e534331d8
delrey919 <151542418+delrey919@users.noreply.github.com>
2024-11-12 15:02:17 +0100

    Creación del header

---
+++ views/templates/header.php
@@ -0,0 +1,15 @@
+<?php
+require_once dirname(__FILE__, 3) . '/config/config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Eventos Sostenibles</title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/bootstrap.min.css">
+</head>
+<body>
+    <nav>
+        <a href="<?= BASE_URL ?>index.php">Inicio</a>
+        <a href="<?= BASE_URL ?>views/user/login.php">Iniciar Sesión</a>
+    </nav>
