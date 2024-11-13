e4268acfbb522e08339754d505540c1328d1d504
AAA <150699364+AlgaeDestructor@users.noreply.github.com>
2024-11-13 16:24:00 +0100

    Creacion de los siguientes controladores

---
+++ controllers/AdminConsejoController.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+require_once '../config/config.php';
+require_once '../models/Consejo.php';
+
+if (!isset($_SESSION['user']) || $_SESSION['user']['rol'] != 'admin') {
+    header('Location: ' . BASE_URL . 'index.php');
+    exit;
+}
+
+$consejoModel = new Consejo($pdo);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = [
+        'titulo' => $_POST['titulo'],
+        'descripcion_breve' => $_POST['descripcion_breve'],
+        'texto_explicativo' => $_POST['texto_explicativo'],
+        'etiquetas' => $_POST['etiquetas']
+    ];
+
+    if (isset($_POST['id']) && !empty($_POST['id'])) {
+        $consejoModel->updateConsejo($_POST['id'], $data);
+    } else {
+        $consejoModel->createConsejo($data);
+    }
+
+    header('Location: ' . BASE_URL . 'admin/consejos_list.php');
+    exit;
+} elseif (isset($_GET['delete'])) {
+    $consejoModel->deleteConsejo($_GET['delete']);
+    header('Location: ' . BASE_URL . 'admin/consejos_list.php');
+    exit;
+}
+?>
